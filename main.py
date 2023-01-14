47dae20ae77c51758f6a25531e5111837914a1ef
Vlad <dogonovv@gmail.com>
2023-01-14 21:00:06 +0200

    f string + countdown

--- main.py
+++ main.py
@@ -13,17 +13,19 @@ class Coffee_Machine:
         for type in self.types_coffee:
             s += 1
             print(f'{s} {type.name}')
+        print(f'Ингридиенты в наличии: {self.ingredients}')
 
     #Выбор кофе
-    def pick_coffee(self):
+    def pick_coffee(self, sec):
         q = -1
         for type in self.types_coffee:
             q += 1
             print(f'{q} {type.name}')
         pick = input(f'Выбирите номер кофе из списка: ')
-        print(f'Готовлю {self.types_coffee[int(pick)].name}')
-        countdown(7)
-        print(f'Ваш {self.types_coffee[int(pick)].name} готов.\nМожете забрать стаканчик.')
+        your_pick = self.types_coffee[int(pick)]
+        print(f'Готовлю {your_pick.name}. До приготовления {sec} секунд.')
+        countdown(sec)
+        print(f'Ваш {your_pick.name} готов.\nМожете забрать стаканчик.')
         
 
 class Coffee:
@@ -38,6 +40,6 @@ americano = Coffee('Americano', {'Water : 1', 'Coffee : 1'})
 espresso = Coffee('Espresso', {'Water : 1', 'Coffee : 1'})
 irish = Coffee('Irish Coffee', {'Water : 1', 'Coffee : 1', 'Whiskey : 1'})
 
-simple_machine = Coffee_Machine('Simple_Machine', [latte, cappuccino, espresso, americano, irish], {'Water : 20', 'Coffee : 20', 'Milk : 15', 'Wiskey : 5'})
+simple_machine = Coffee_Machine('Simple_Machine', [latte, cappuccino, espresso, americano, irish], ingredients = {'Water : 20', 'Coffee : 20', 'Milk : 15', 'Wiskey : 5'})
 
-simple_machine.pick_coffee()
+simple_machine.available_coffee()
